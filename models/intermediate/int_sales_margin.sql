3ea0a9b52cac875dba904bb09d8ba052c93c24c6
JUAN TORRES <juantorresj18@gmail.com>
2024-05-08 16:20:30 +0000

    made a join between sales and products

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,13 @@
+
+SELECT 
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64) AS purchase_price, 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }} s
+LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }} p 
+    USING (products_id)
